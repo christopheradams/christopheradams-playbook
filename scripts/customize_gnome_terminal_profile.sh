b3896ac7dd3dd1e8c497b2feafd59aec51439940
Christopher Adams <christopher@fabricatorz.com>
2020-01-14 15:38:50 +0800

    Disable gnome-terminal background transparency

---
+++ scripts/customize_gnome_terminal_profile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dconfdir=/org/gnome/terminal/legacy/profiles:
+
+source scripts/get_gnome_terminal_profile_uuid.sh
+
+id=$(get_profile_uuid "$1")
+
+dconf write $dconfdir/:$id/use-theme-transparency false
+dconf write $dconfdir/:$id/use-transparent-background true
+dconf write $dconfdir/:$id/background-transparency-percent 0
