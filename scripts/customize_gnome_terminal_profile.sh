2d6846396643e44dbdda86a2fe5d4d1932038f0d
Christopher Adams <christopher@fabricatorz.com>
2020-01-14 16:13:57 +0800

    Set gnome-terminal cursor color

--- scripts/customize_gnome_terminal_profile.sh
+++ scripts/customize_gnome_terminal_profile.sh
@@ -8,3 +8,4 @@ id=$(get_profile_uuid "$1")
 dconf write $dconfdir/:$id/use-theme-transparency false
 dconf write $dconfdir/:$id/use-transparent-background true
 dconf write $dconfdir/:$id/background-transparency-percent 0
+dconf write $dconfdir/:$id/cursor-background-color "'rgb(171,70,66)'"
